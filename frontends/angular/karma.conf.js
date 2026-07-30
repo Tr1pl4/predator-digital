aa70c6e39e9bc6aad83e68cc6a7b8fba97c0002b
BBalintB <balint.bota@propairflight.com>
2026-07-30 10:56:15 +0200

    fix: karma.conf.js was incomplete, solely focused on ci/cd compatibility, missing parts are filled in

--- frontends/angular/karma.conf.js
+++ frontends/angular/karma.conf.js
@@ -5,11 +5,35 @@ module.exports = function (config) {
   const isCI = !!process.env.CI;
 
   config.set({
-    // ...
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {},
+      clearContext: false
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/angular'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }]
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
     autoWatch: !isCI,
+    browsers: isCI ? ['ChromeHeadlessNoSandbox'] : ['Chrome'],
     singleRun: isCI,
     restartOnFileChange: !isCI,
-    browsers: isCI ? ['ChromeHeadlessNoSandbox'] : ['Chrome'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
